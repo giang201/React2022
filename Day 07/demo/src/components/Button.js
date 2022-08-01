e6e6cf6d001a3ef5ae909951ae9b4541def439f6
Đặng Hoàng Giang <109504653+giang201@users.noreply.github.com>
2022-08-01 14:51:18 +0700

    Signed-off-by: Đặng Hoàng Giang <109504653+giang201@users.noreply.github.com>

--- Day 07/demo/src/components/Button.js
+++
@@ -1,5 +0,0 @@
-export default function Button(props) {
-    return (
-        <button className={props.className} value={props.value} onClick={props.onClick} type={props.type}>{props.labelName}</button>
-    )
-}
